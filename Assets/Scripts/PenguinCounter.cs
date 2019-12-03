7e644ea005177086ea8ddae8aad6bc51b54b7b1c
Dusteen99 <truesk8er01@gmail.com>
2019-12-03 18:23:50 -0500

    Penguin counting script

---
+++ Assets/Scripts/PenguinCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinCounter : MonoBehaviour
+{
+    public int Penguins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Penguins = GameObject.FindGameObjectsWithTag("Penguins").Length;
+    }
+}
