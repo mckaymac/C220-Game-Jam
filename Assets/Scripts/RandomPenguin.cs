883388d1fa8e82e91d1fef1069b5fc07fb1712bc
Dusteen99 <truesk8er01@gmail.com>
2019-12-03 17:29:09 -0500

    Finished the random penguin script/prefab

--- Assets/Scripts/RandomPenguin.cs
+++ Assets/Scripts/RandomPenguin.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class RandomPenguin : MonoBehaviour
 {
     //Used to generate which direction the penguin goes
-    Random rand = new Random();
+    System.Random rand = new System.Random();
+    Vector3 Direction = new Vector3(0,0,0);
 
     //How often the penguin should move, counter is used to count up to Cycle
     private int Cycle = 120;
@@ -14,12 +14,17 @@ public class RandomPenguin : MonoBehaviour
 
     //Used for distance to player for text that pops up
     public GameObject Player;
-    private float Distance;
+    public float Distance;
     public GameObject CommandKey;
     public GameObject Command;
 
     //Used to trigger animations for the pengiun
     private Animator Animator;
+    private Rigidbody Rigidbody;
+
+    //Stuff for penguin physics
+    public float movementSpeed = 3;
+    private bool Moving = false;
     
    
 
@@ -28,34 +33,66 @@ public class RandomPenguin : MonoBehaviour
     {
         Animator = gameObject.GetComponent<Animator>();
         counter = 0;
+        Direction = new Vector3(rand.Next(180), 0, rand.Next(180));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Walking animation triggered while moving
+        if(Moving){
+            Animator.SetInteger("Walk", 1);
+        }
+        else{
+            Animator.SetInteger("Walk", 0);
+        }
+        
         counter += 1;
         if(counter >= Cycle){
-            gameObject.transform.position += new Vector3(1, 0, 0);
+            Moving = true;
+
+            //Face random direction, only do this once per movement
+            if(counter == Cycle){
+                gameObject.transform.Rotate(new Vector3(0, rand.Next(0, 360),0), Space.World);
+            }
+            
+            //Move a little each update given the random direction
+            gameObject.transform.position += gameObject.transform.forward / 25;
+            
+            //gameObject.transform.Translate(Direction, Space.World);
 
         }
+        else{
+            Moving = false;
+        }
+
+        if(counter == 2 * Cycle){
+            counter = 0;
+        }
+
     }
 
     void Update(){
-        //Calculating distance between door and player
+        //Calculating distance between penguin and player
         var xDiff = gameObject.transform.position.x - Player.transform.position.x;
         var zDiff = gameObject.transform.position.z - Player.transform.position.z;
         Distance = Mathf.Pow(Mathf.Pow(xDiff, 2) + Mathf.Pow(zDiff, 2), (float) 0.5);
     }
 
+
     void OnMouseOver(){
         //When within 2 units, show door instructions and allow them to teleport
-        if(Distance < 2){
+        if(Distance < 4){
             //Showing the texts on canvas and changes the text
             Command.GetComponent<Text>().text = "Rescue Penguin";
+            CommandKey.GetComponent<Text>().text = "[E]";
             CommandKey.SetActive(true);
             Command.SetActive(true);
             if(Input.GetButtonDown("Action") ){
-                //Triggers animations and disables the box collider
+                Destroy(gameObject);
+                CommandKey.SetActive(false);
+                Command.SetActive(false);
+                //Probably add something here the decreases the meter
             }
         }
         else{
