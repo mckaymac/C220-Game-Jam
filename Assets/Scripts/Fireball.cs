9df752f53e8f6502d4df4d7b7be581ae37d6749c
Kevin Cao <kcao1998@gmail.com>
2019-12-03 20:39:56 -0500

    Completed fireball spawning

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public GameObject explosionObject; // Choose explosion effect.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject explosion = Instantiate(explosionObject, transform.position, Quaternion.identity, transform.parent) as GameObject;
+        Destroy(gameObject);
+        Destroy(explosion, 1.3f);
+    }
+}
---
+++ Assets/Scripts/SpawnFireballs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFireballs : MonoBehaviour
+{
+    [Tooltip("Drag the fireball object here.")]
+    public GameObject fireballObject; // Drag the fireball object here.
+    [Tooltip("The radius in which the spawner can spawn fireballs.")]
+    public int range = 10; // The radius in which the spawner can spawn fireballs.
+    [Tooltip("The frequency in which the fireballs should spawn in seconds.")]
+    public float frequency = 1; // The frequency in which fireballs should spawn in seconds.
+    private float timeSoFar = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSoFar += Time.deltaTime;
+
+        if (timeSoFar >= frequency)
+        {
+            spawnRandomFireball();
+            timeSoFar = 0;
+        }
+    }
+
+    void spawnRandomFireball()
+    {
+        int randomX = Random.Range(0, range);
+        int randomZ = Random.Range(0, range);
+        GameObject fireball = Instantiate(fireballObject, transform.position + new Vector3(randomX, 0, randomZ), Quaternion.identity, transform);
+    }
+}
