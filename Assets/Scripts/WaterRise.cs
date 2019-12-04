d1434d2205e16278736d9a483137b8699c052a99
inewhart <inewhart@iu.edu>
2019-12-04 00:09:19 -0500

    Added water rise

---
+++ Assets/Scripts/WaterRise.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRise : MonoBehaviour
+{
+    public GameObject Water;
+    public GameObject Ice;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        while(i < 100)
+        {
+            i++;
+            animator.Play("WaterRise");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
